99734ff733e8aab3e27627672f12fa127fc8c81a
mahmud jamal <mahmudjamal08@gmail.com>
2021-02-08 00:42:57 +0700

    Added swaggerUI, yet configured.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const todoRoute = require("./routes/todoRoute");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 app.use(express.json()); // req.body
 
 // routes
+app.use("/", swaggerUi.serve);
+app.get("/", swaggerUi.setup(swaggerDocument));
 app.use(todoRoute);
 
 app.listen(3000, () => {
